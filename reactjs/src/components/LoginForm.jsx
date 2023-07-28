05a15688859437de24fb2f3fc5b8f24e7dd15986
abc123 <pjb1202@nate.com>
2023-07-28 13:03:24 +0900

    Backend boilerplate ( cookie , token )

--- reactjs/src/components/LoginForm.jsx
+++ reactjs/src/components/LoginForm.jsx
@@ -1,6 +1,66 @@
-import React from 'react'
+import React, { useState, useRef, useContext, useEffect } from 'react'
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 function LoginForm() {
+
+  // 쿠키 만들어주는 함수
+  const [cookie , setCookie] = useCookies(['token']);
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // 서버로 전송할 로그인 데이터
+    const data = {
+      email: formData.email,
+      password: formData.password,
+    };
+
+    const serverUrl = 'http://localhost:8091/api/auth/login';
+
+    const dataJson = JSON.stringify(data);
+    console.log('dataJson', dataJson);
+
+    fetch(serverUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: dataJson,
+    })
+    .then(res => {
+      console.log('res.status: ', res.status);
+      if (res.status !== 200) {
+        return alert('로그인에 실패하였습니다. 다시 시도해주세요~');
+      }
+
+      return res.json();
+    })
+    .then(data => {      
+      if (data) {
+        
+        // 쿠키에 token 값 저장
+        const expireTimeDate = new Date(Number(data.tokenExpiresIn));
+        console.log('expireTime: ', expireTimeDate.toLocaleString());
+        setCookie('token', data.accessToken, { path: '/', expires: expireTimeDate });
+
+        window.location.href = '/thejoeun1-boilerplate1';
+      }
+    });
+  };
+
+  // console.log(formData)
+
   return (
     <section className="d-flex vh-100">
       <div className="container-fluid row justify-content-center align-content-center">
@@ -8,19 +68,31 @@ function LoginForm() {
           <div className="card-body p-5 text-center">
             <h2 className="text-white">LOGIN</h2>
             <p className="text-white-50 mt-2 mb-5">서비스를 사용하려면 로그인을 해주세요!</p>
-            <div className = "mb-2">
-              <form action="/login" method="POST">
+            <div className="mb-2">
+              <form action="/login" onSubmit={handleSubmit}>
                 <div className="mb-3">
                   <label className="form-label text-white">Email address</label>
-                  <input type="email" className="form-control" name="username" />
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
                 </div>
                 <div className="mb-3">
                   <label className="form-label text-white">Password</label>
-                  <input type="password" className="form-control" name="password" />
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                  />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
               </form>
-              <button type="button" className="btn btn-secondary mt-3" >회원가입</button>
+              <button type="button" className="btn btn-secondary mt-3">회원가입</button>
             </div>
             <div>
               <a href="http://localhost:8091/oauth2/authorization/google">
--- reactjs/src/components/SignUpForm.jsx
+++ reactjs/src/components/SignUpForm.jsx
@@ -1,6 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SignUpForm() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    nickname: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // 서버로 전송할 회원 가입 데이터
+    const data = {
+      email: formData.email,
+      password: formData.password,
+      nickname: formData.nickname
+    };
+
+    const serverUrl = 'http://localhost:8091/api/auth/signup';
+
+    fetch(serverUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+      // 서버로부터 온 응답 처리
+      console.log(result);
+
+           // 회원 가입 성공 메시지를 띄우고 로그인 페이지로 이동
+           window.alert('회원 가입에 성공했습니다.');
+           window.location.replace('/thejoeun1-boilerplate1/login');
+
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
+
+  // console.log(formData);
+
   return (
     <section className="d-flex vh-100">
       <div className="container-fluid row justify-content-center align-content-center">
@@ -9,19 +57,37 @@ function SignUpForm() {
             <h2 className="text-white">SIGN UP</h2>
             <p className="text-white-50 mt-2 mb-5">서비스 사용을 위한 회원 가입</p>
             <div className = "mb-2">
-              <form method="POST">
+              <form onSubmit={handleSubmit}>
                 <input type="hidden"/>
                 <div className="mb-3">
                   <label className="form-label text-white">Email address</label>
-                  <input type="email" className="form-control" name="email" />
+                  <input 
+                    type="email" 
+                    className="form-control" 
+                    name="email" 
+                    value={formData.email}
+                    onChange={handleInputChange} 
+                  />
                 </div>
                 <div className="mb-3">
                   <label className="form-label text-white">Password</label>
-                  <input type="password" className="form-control" name="password" />
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password" 
+                    value={formData.password}
+                    onChange={handleInputChange}
+                  />
                 </div>
                 <div className="mb-3">
                   <label className="form-label text-white">nickname</label>
-                  <input type="text" className="form-control" name="nickname" />
+                  <input
+                    type="text" 
+                    className="form-control"
+                    name="nickname"
+                    value={formData.nickname}
+                    onChange={handleInputChange} 
+                  />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
               </form>
